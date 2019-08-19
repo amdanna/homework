6f7bf4f6cfeb904cc724b62263573d5cfd4b73cf
Amanda <danna.amanda@gmail.com>
2019-08-19 14:37:24 -0400

    Made Queue class

---
+++ W6D1/queue_class.rb
@@ -0,0 +1,23 @@
+class Queue
+
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue.first
+    end
+
+    def print_queue
+        @queue
+    end
+
+end
