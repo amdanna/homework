6d07f572dfd0b72e1f8802e135dc06b928d62ad9
Amanda <danna.amanda@gmail.com>
2019-08-28 12:57:09 -0400

    more daily js practice

---
+++ W7D3/dog_years.js
@@ -0,0 +1,15 @@
+//Dog years...created var for my age.
+const myAge = 39;
+//created first 2 years
+let earlyYears = 2
+
+//accounts
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+//rest of dog years after early years.
+laterYears *= 4;
+dogYears = (earlyYears + laterYears);
+let myName = 'Amanda'.toLowerCase();
+//statement for human years and dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogYears} years old in dog years.`)
+
