b610ab99e3589cffb0759255e9c55615d5a14283
Amanda <danna.amanda@gmail.com>
2019-08-30 13:31:51 -0400

    started practice

---
+++ W7D5/practice.js
@@ -0,0 +1,100 @@
+// The scope of `random` is too loose 
+
+
+
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+      return 'Marathon';
+    } else if (random === 1) {
+      return 'Triathlon';
+    } else if (random === 2) {
+      return 'Pentathlon';
+    }
+};
+  
+  // The scope of `days` is too tight 
+const getTrainingDays = event => {
+      let days = 0;
+    if (event === 'Marathon') {
+      days = 50;
+    } else if (event === 'Triathlon') {
+      days = 100;
+    } else if (event === 'Pentathlon') {
+      days = 200;
+    }
+  
+    return days;
+};
+  
+  // The scope of `name` is too tight 
+  
+const logEvent = (name, event) => {
+    
+    console.log(`${name}'s event is: ${event}`);
+};
+  
+  
+const logTime = (name, days) => {
+    console.log(`${name}'s time to train is: ${days} days`);
+};
+const name = 'Nala';
+const event = getRandEvent();
+const days = getTrainingDays(event);
+  // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+  
+  
+logEvent(name, event);
+logTime(name, days);
+  
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
+  
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+
+chores.push('clean floors', 'wipe windows');
+
+console.log(chores);
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+
+chores.pop();
+console.log(chores);
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+groceryList.unshift('popcorn');
+console.log(groceryList.slice(1,4));
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+
+function removeElement(newArr){
+  newArr.pop();
+}
+
+removeElement(concept);
+console.log(concept);
+
+const numberClusters = [[1,2],[3,4],[5,6]];
+const target = numberClusters[2][1];
---
+++ W7D5/secret_message.js
@@ -0,0 +1,7 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 
+'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 
+'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+function removeLastString(secretMessage) {
+    secretMessage.pop();
+}
